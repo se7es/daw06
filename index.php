482d31ebc12464f9d4c05052175ac124e1d192d0
se7es <se7@hotmail.es>
2024-02-06 12:12:45 +0000

    confirmado

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>daw06</title>
+</head>
+<body>
+    <h1>daw06</h1>
+    <p>sergio lopez continente</p>
+    <p>25191540p</p>
+</body>
+</html>
